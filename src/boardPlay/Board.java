25d5299d77c23244eadce44ea4e8571ad7b4e43e
Nishi790 <rebelnishi@gmail.com>
2016-03-19 18:24:15 -0400

    Added Camera controller

--- src/boardPlay/Board.java
+++ src/boardPlay/Board.java
@@ -6,26 +6,27 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
-public class Board extends BasicGameState{
+public class Board extends BasicGame implements Runnable{
 	TiledMap board;
 	Hashtable<Integer,Nodes> nodes;
 	static final int ID=0;
 	Pieces[] players=new Pieces[4];
 	File map;
+	CameraController camera;
 	
-	public Board(File map){
+	
+	public Board(String name,File map){
+		super(name);
 		this.map=map;
 	}
 
-	@Override
-	public int getID() {
-		return ID;
-	}
-
-	public void init(GameContainer app, StateBasedGame game) throws SlickException {
+	public void init(GameContainer app) throws SlickException {
 		try{
 			board= new TiledMap("/Assets/board.tmx");
 			nodes=Nodes.getConnections(map);
@@ -33,6 +34,7 @@ public class Board extends BasicGameState{
 			players[1]=new Pieces(new Image("/Assets/P2.png"),1,nodes,15);
 			players[2]=new Pieces(new Image("/Assets/P2.png"),1,nodes,-5);
 			players[3]=new Pieces(new Image("/Assets/P1.png"),1,nodes,-15);
+			camera=new CameraController(board, app);
 			
 		}
 		catch (Exception e) {
@@ -43,17 +45,35 @@ public class Board extends BasicGameState{
 	}
 
 
-	public void render(GameContainer app, StateBasedGame game, Graphics g) throws SlickException {
-		board.render(0,0);
+	public void render(GameContainer app, Graphics g) throws SlickException {
 		for(Pieces a:players){
 			a.getAvatar().draw(a.getXLocation(),a.getYLocation());
+			}
+		camera.drawMap(0, 0);
 		}
 
+	@Override
+	public void update(GameContainer app, int delta) throws SlickException {
+		camera.centerOn(players[0].getXLocation(),players[0].getYLocation());
+		//if you get input from the server...
+		
 	}
 
-	public void update(GameContainer app, StateBasedGame game,int delta) throws SlickException {
-		// TODO Auto-generated method stub
-		
+	@Override
+	public void run() {
+		try {
+			DisplayMode[] possibilities=Display.getAvailableDisplayModes();
+			for(DisplayMode d:possibilities){
+				int width=d.getWidth();
+				int height=d.getHeight();
+				System.out.println(Integer.toString(width)+"x"+Integer.toString(height));
+			}
+			AppGameContainer app=new AppGameContainer(new Board("Party Plaza",new File("Assets/MapDesc.txt")),1280,720,true);
+			app.start();
+		} 
+		catch (SlickException | LWJGLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/client/PartyPlaza.java
+++ src/client/PartyPlaza.java
@@ -1,9 +1,14 @@
 package client;
 
+import java.io.File;
+
+import boardPlay.Board;
+
 public class PartyPlaza {
 
 	public static void main(String[] args) {
-
+		Board mainBoard=new Board("Party Plaza", new File("Assets/MapDesc.txt"));
+		mainBoard.run();
 	}
 
 }
